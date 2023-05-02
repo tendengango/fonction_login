9ebe9ca9a56214cc11449bee77cd4d438d113fea
tendengango <framboutel@gmail.com>
2023-05-02 15:45:50 +0100

    test

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: %w(show edit update destroy)
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to tasks_path
+      else
+        render :new
+      end
+    end
+
+    def show
+    end
+
+    def  edit
+
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to @user
+      else
+        render :edit
+      end
+    end
+
+    def  destroy
+      @user.destroy
+      redirect_to new_user_path
+    end
+
+    private
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end     
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password  
+end
